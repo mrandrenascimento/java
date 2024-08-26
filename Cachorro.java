e05f5a09fead507076a3b3e66219d70fa59cadaf
mrandrenascimento <159596002+mrandrenascimento@users.noreply.github.com>
2024-08-26 09:12:04 -0300

    Add files via upload

---
+++ Cachorro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.preincipal;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Cachorro implements Animal {
+
+    @Override
+    public String emitirSom() {
+        return "Auau!";
+    }
+    
+}
--- Principal.java
+++ Principal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.principal;
+package com.mycompany.preincipal;
 
 /**
  *
@@ -11,16 +11,10 @@ package com.mycompany.principal;
 public class Principal {
 
     public static void main(String[] args) {
-       
-  
-        Cliente cliente = new Cliente("45188", Sexo.FEMININO, Estando_Civil.CASADO, "150594", "Euclides", "39 8974542", "Euclidees@google.com", 
-                new Endereco("Condominio", "45 E", "Condominio", "451212154", "Silveira Mar", Unidade_Federetiva.BAHIA));
+        Cachorro toto = new Cachorro();
+        Gato gato = new Gato();
         
-        Prestacao_Servico prestacaoServico= new Prestacao_Servico("56414", "56415", "451215457", "4454724", "Antonio", "351457451", "Antonio@google.com", 
-                new Endereco("Comercio", "45", "Comercio", "412124554", "Santos", Unidade_Federetiva.BAHIA));
-        
-        System.out.println(prestacaoServico.toString());
-        System.out.println(cliente.toString());
-        
+        System.out.println("Som: " + toto.emitirSom());
+        System.out.println("Som: " + gato.emitirSom());
     }
 }
